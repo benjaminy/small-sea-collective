73b6c5324fcf311c323ac2829179f29df8064cd7
Benjamin Ylvisaker <benjaminy@fastmail.fm>
2025-09-18 13:59:02 -0600

    some stubs

--- Source/small_sea_cli.py
+++ Source/small_sea_cli.py
@@ -15,8 +15,7 @@ def cli(ctx, verbose, hub_port):
     """ Small Sea Core CLI tool """
     ctx.ensure_object(dict)
     ctx.obj["verbose"] = verbose
-    ctx.obj["hub_part"] = hub_port
-    click.echo(f"CLI CALLED")
+    ctx.obj["hub_port"] = hub_port
 
 
 @cli.command()
@@ -34,6 +33,8 @@ def new_participant(ctx, nickname, cloud_backend, cloud_url):
 
     if (cloud_backend is None) != (cloud_url is None):
         click.echo("ERROR. Only one of backend and url provided")
+    elif cloud_url is not None:
+        click.echo("SHOULD ADD CLOUD")
 
 
 @cli.command()
@@ -42,9 +43,11 @@ def new_participant(ctx, nickname, cloud_backend, cloud_url):
 @click.pass_context
 def open_session(ctx, nickname, team_name ):
     def encode_sessions( ss ):
+        """ First serialize the keys, then the whole dict """
         step1 = { json.dumps(k): v for k, v in ss.items() }
         return json.dumps(step1)
     def decode_sessions( ss_str ):
+        """ First deserialize the dict, then the keys """
         step1 = json.loads(ss_str)
         return { json.loads(k): v for k, v in step1 }
 
@@ -96,7 +99,35 @@ def create_new_team(ctx, nickname, team_name ):
     small_sea.create_new_team( session, team_name )
 
 
+@cli.command()
+@click.argument("nickname")
+@click.argument("team_name")
+@click.option("--abort_on_stale", is_flag=True, help="If the local copy is stale, abort instead of automatically reconciling")
+@click.pass_context
+def upload_changes(ctx, nickname, team_name):
+    small_sea = SmallSeaLib.SmallSeaClient()
+    session = open_session(nickname, team_name)
+    small_sea.upload_changes(session)
+
+
+@cli.command()
+@click.argument("nickname")
+@click.pass_context
+def make_device_link_invitation(ctx, nickname):
+    pass
+
+
+@cli.command()
+@click.argument("nickname")
+@click.argument("team_name")
+@click.argument("invitee")
+@click.pass_context
+def make_team_invitation(ctx, nickname, team_name, invitee):
+    pass
+
+
 if __name__ == "__main__":
     if False:
         print("Registered commands:", list(cli.commands.keys()))
-    cli(prog_name="Small Sea Core CLI")
+    # prog_name="Small Sea Core CLI"
+    cli()
